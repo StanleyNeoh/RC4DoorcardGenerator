84591d39be5c9cf9428421c373acba2dc74b69b1
StanleyNeoh <95394480+StanleyNeoh@users.noreply.github.com>
2023-01-28 02:51:37 +0800

    Update project to .py instead

---
+++ main.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import re
+import os
+from PIL import Image
+from logging import PlaceHolder
+from pptx import Presentation
+from pptx.util import Pt
+
+NameCol = 'Name'
+DisplayCol = 'Name (to be displayed on doorcard)'
+YearCol = 'Year'
+MajorCol = 'Major'
+CaptionCol = 'Doorcard Caption'
+ExcelLocation = './doorcards.xlsx'
+
+TemplateLocation = './templates/URSA_Door_card.pptx'
+FontLocation = './font/DINCondensed-Regular.ttf'
+
+PhotoLocation = './doorcard_photos'
+PptxDestination = './doorcards_pptx'
+
+def ProcessField(s):
+    return re.sub(r'(?<!\w)and(?!\w)',"&", s).upper()
+
+def GetFileName(name):
+    name = name[:20].strip()
+    picArr = os.listdir(PhotoLocation)
+    filename = []
+    for x in picArr:
+        searched = re.search(name, x)
+        if searched:
+            filename.append(x)
+    if len(filename) == 0:
+        print("No picture found for {0}".format(name))
+        return None
+    if len(filename) > 1:
+        print("Ambiguity in filename for {0}. Choosing first path".format(name), filename)
+    return filename[0]
+
+def PrimePics():
+    imagenames = os.listdir(PhotoLocation)
+    for imagename in imagenames:
+        path = os.path.join(PhotoLocation, imagename)
+        try:
+            im = Image.open(path)
+            im.save(path)
+        except Exception as e:
+            print(e)
+
+def CreateDoorcard(name, data_dict):
+    prs = Presentation(TemplateLocation)
+    phs = prs.slides[0].placeholders
+    for ph in phs:
+        if ph.name == "Picture":
+            ph.insert_picture(os.path.join(PhotoLocation, GetFileName(name)))
+        else:
+            ph.text = ProcessField(data_dict[ph.name])
+
+    safeName = re.sub(r'[^A-z]', "", name) + "_Ursa.pptx"
+    if not os.path.exists(PptxDestination):
+        os.makedirs(PptxDestination)
+    prs.save(os.path.join(PptxDestination, safeName))
+
+if __name__ == "__main__":
+    df = pd.read_excel(ExcelLocation)
+    PrimePics()
+    for i, row in df.iterrows():
+        try:
+            CreateDoorcard(
+                row[NameCol], 
+                {
+                    "Name": row[DisplayCol], 
+                    "Year": "1", #row[YearCol], 
+                    "Major": row[MajorCol], 
+                    "Caption": row[CaptionCol]
+                }
+            )
+        except Exception as e:
+            print("Error at {0}:".format(row[NameCol]), e)
